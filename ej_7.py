0807f3cfcc27e9030746697ce7fa5606caca56a6
nanoflores888 <112973319+nanoflores888@users.noreply.github.com>
2023-03-21 18:52:46 -0300

    CORRECIONES
    Correciones de acuerdo a los visto en la clase 07

--- ej_7.py
+++ ej_7.py
@@ -3,27 +3,33 @@ class Cuenta():
         self.__titular = titular
         self.__edad = edad
         self.__cantidad = cantidad
-        
+    
+    @property    
     def getTitular(self):
         return self.__titular
     
+    @property    
     def setTitular(self, titular):
         self.__titular = titular
-        
+    
+    @property    
     def getEdad(self):
         return self.__edad
     
+    @property
     def setEdad(self, edad):
         self.__edad = edad
-        
+    
+    @property    
     def getCantidad(self):
         return self.__cantidad
     
+    @property
     def setCantidad(self, dinero):
         self.__cantidad = dinero
         
     def mostrar(self):
-        print(f'Persona : {self.getTitular()}, Cantidad : {self.getCantidad()}, Edad : {self.getEdad()}')
+        print(f'Persona : {self.getTitular}, Cantidad : {self.getCantidad}, Edad : {self.getEdad}')
         
     def ingresar(self, cantidad):
         if cantidad > 0:
@@ -34,9 +40,7 @@ class Cuenta():
 
 #Pruebas no parte del ejercicio.
 if __name__ == "__main__": 
-    p1 = Cuenta()
-    p1.setTitular('Carlos')
-    p1.setEdad(20)
+    p1 = Cuenta('Carlos', 20)
     print(p1.mostrar())
     p1.ingresar(2000)
     print(p1.mostrar())
